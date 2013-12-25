f06f267b76ce66358b822837db2b5f2acd3dcf1b
posaunehm <posaunehm@gmail.com>
2013-12-26 01:00:08 +0900

    Add error handling

--- DocxReader/DocXReader.cs
+++ DocxReader/DocXReader.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Novacode;
+﻿using Novacode;
 
 namespace DocxReader
 {
     public class DocXReader
     {
-        private DocX _doc;
+        private readonly DocX _doc;
 
         public DocXReader(string path)
         {
@@ -30,36 +26,5 @@ namespace DocxReader
             }
         }
 
-    }
-
-    public class DocumentPropertyStore
-    {
-        private Dictionary<string, string> _coreProperties;
-
-        public DocumentPropertyStore(Dictionary<string, string> coreProperties)
-        {
-            _coreProperties = coreProperties;
-        }
-
-        //[0]: {[dc:title, Title]}
-        //[1]: {[dc:subject, サブタイトル]}
-        //[2]: {[dc:creator, posaunehm]}
-        //[3]: {[cp:keywords, Keyword]}
-        //[4]: {[dc:description, ]}
-        //[5]: {[cp:lastModifiedBy, 前川博志]}
-        //[6]: {[cp:revision, 5]}
-        //[7]: {[dcterms:created, 2013-12-25T13:00:00Z]}
-        //[8]: {[dcterms:modified, 2013-12-25T13:54:00Z]}
-        //[9]: {[cp:category, 分類]}
-        //[10]: {[cp:contentStatus, 状態]}
-
-        public string Title { get { return _coreProperties["dc:title"]; } }
-        public string Creator { get { return _coreProperties["dc:creator"]; } }
-        public string SubTitle { get { return _coreProperties["dc:subject"]; } }
-        public string Keywords { get { return _coreProperties["cp:keywords"]; }}
-        public string Description { get { return _coreProperties["dc:description"]; } }
-        public string Category { get { return _coreProperties["cp:category"]; } }
-        public string Status { get { return _coreProperties["cp:contentStatus"]; } }
-        public DateTime CreatedDate { get { return DateTime.Parse(_coreProperties["dcterms:created"]); } }
     }
 }
---
+++ DocxReader/DocumentPropertyStore.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DocxReader
+{
+    public class DocumentPropertyStore
+    {
+        private readonly Dictionary<string, string> _coreProperties;
+
+        public DocumentPropertyStore(Dictionary<string, string> coreProperties)
+        {
+            _coreProperties = coreProperties;
+        }
+
+        private string GetOrDefault(string name)
+        {
+            return _coreProperties.ContainsKey(name) ? GetOrDefault(name) : null;
+        }
+
+        public string Title
+        {
+            get
+            {
+                return GetOrDefault("dc:title") ?? "";
+            }
+        }
+
+        public string Creator
+        {
+            get
+            {
+                return GetOrDefault("dc:creator") ?? "";
+            } 
+        }
+
+        public string SubTitle
+        {
+            get
+            {
+                return GetOrDefault("dc:subject") ?? "";
+            }
+        }
+
+        public string Keywords
+        {
+            get
+            {
+                return GetOrDefault("cp:keywords") ?? "";
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return GetOrDefault("dc:description") ?? "";
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return GetOrDefault("cp:category") ?? "";
+            }
+        }
+
+        public string Status
+        {
+            get
+            {
+                return GetOrDefault("cp:contentStatus") ?? "";
+            }
+        }
+
+        public DateTime CreatedDate
+        {
+            get
+            {
+                var createdStr = GetOrDefault("dcterms:created");
+                return createdStr == null ? DateTime.MinValue : DateTime.Parse(createdStr);
+            }
+        }
+    }
+}
