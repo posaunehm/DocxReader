c8c3cb7680db7e068f91ac4ea0b71b69754f6afb
posaunehm <posaunehm@gmail.com>
2013-12-25 22:26:08 +0900

    Initial Commit
    SImply Read Docx Files using docx Library

---
+++ DocxReader/DocXReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Novacode;
+
+namespace DocxReader
+{
+    public class DocXReader
+    {
+        private DocX _doc;
+
+        public DocXReader(string path)
+        {
+            _doc = DocX.Load(path);
+        }
+
+        public string Text {
+            get
+            {
+                return _doc.Text;
+            }
+        }
+    }
+}
---
+++ Tests/DocXReaderTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DocxReader;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DocXReaderTest
+    {
+        [TestCase]
+        public void ファイルをオープンしテキストを出力する()
+        {
+            var sut = new DocXReader(@"Data\Hello World.docx");
+
+            sut.Text.Is("Hello World!");
+        }
+
+        [TestCase]
+        public void ファイルをオープンしテキストを出力する_()
+        {
+            var sut = new DocXReader(@"Data\FooBar.docx");
+
+            sut.Text.Is("FooBar");
+        }
+    }
+}
